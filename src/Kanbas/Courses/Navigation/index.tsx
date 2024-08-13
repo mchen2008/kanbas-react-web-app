31e42eab80e0d07aace70d1569130d537cd82b05
mchen2008 <35387850+lusystemsbio@users.noreply.github.com>
2024-08-12 23:51:58 -0400

    a6

--- src/Kanbas/Courses/Navigation/index.tsx
+++ src/Kanbas/Courses/Navigation/index.tsx
@@ -3,21 +3,21 @@ import "./index.css";
 import { Navigate, Route, Routes, useParams, useLocation } from "react-router";
 export default function CoursesNavigation() {
 
-   const links = ["Home", "Modules", "Piazza", "Zoom", "Assignments", "Quizzes", "Grades", "People"];
-   const { pathname } = useLocation();
+  const links = ["Home", "Modules", "Piazza", "Zoom", "Assignments", "Quizzes", "Grades", "People"];
+  const { pathname } = useLocation();
 
-   const pathStrSplit = pathname.split('/')
-   const id = pathStrSplit[3]
- 
+  const pathStrSplit = pathname.split('/')
+  const id = pathStrSplit[3]
+
   return (
-    <div id="wd-courses-navigation"  className="list-group fs-5 rounded-0
+    <div id="wd-courses-navigation" className="list-group fs-5 rounded-0
      position-fixed d-none d-md-block z-2"  style={{ width: 120 }} >
-   
 
-{links.map((link) => (
+
+      {links.map((link) => (
         <Link to={`/Kanbas/Courses/${id}/${link}`} className={`list-group-item border border-0 p-0
               ${pathname.includes(link) ? "black border-left border-dark" : "text-danger"}`}>
-       
+
           <br />
           {link}
         </Link>
@@ -25,7 +25,7 @@ export default function CoursesNavigation() {
 
 
 
-   </div>
+    </div>
 
   );
 }
