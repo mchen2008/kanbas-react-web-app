cb52b8330d1e064bac9a8cc38edb9bd261b4392d
mchen2008 <35387850+lusystemsbio@users.noreply.github.com>
2024-08-16 11:27:01 -0400

    test

--- src/Kanbas/Courses/Modules/client.tsx
+++ src/Kanbas/Courses/Modules/client.tsx
@@ -2,6 +2,7 @@ import axios from "axios";
 const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 const COURSES_API = `${REMOTE_SERVER}/api/courses`;
 
+//console.log("remote_server:", REMOTE_SERVER)
 export const findModulesForCourse = async (courseId: string) => {
     //console.log("check module api", `${COURSES_API}/${courseId}/modules`)
   const response = await axios
--- src/Kanbas/Courses/People/client.tsx
+++ src/Kanbas/Courses/People/client.tsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 export const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 export const USERS_API = `${REMOTE_SERVER}/api/users`;
+console.log("remote_server:", REMOTE_SERVER)
 
 export const findAllUsers = async () => {
   const response = await axios.get(USERS_API);
@@ -23,7 +24,6 @@ export const deleteUser = async (userId: string) => {
   const response = await axios.delete( `${USERS_API}/${userId}` );
   return response.data;
 };
-
 
 export const updateUser = async (user: any) => {
   const response = await axios.put(`${USERS_API}/${user._id}`, user);
