479a7238092669bcf24e50bcb70e4d5d80401018
mchen2008 <35387850+lusystemsbio@users.noreply.github.com>
2024-08-16 16:00:16 -0400

    small fix

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -24,11 +24,11 @@ export default function Kanbas() {
 
   const [course, setCourse] = useState<any>({
     _id: "1234", name: "New Course", number: "New Number",
-    startDate: "2023-09-10", endDate: "2023-12-15",     image: "/images/reactjs.jpg", description: "New Description",
+    startDate: "2023-09-10", endDate: "2023-12-15", image: "/images/reactjs.jpg", description: "New Description",
   });
   const addNewCourse = (async () => {
     const newCourse = await client.createCourse(course);
-    setCourses([ ...courses, newCourse ]);
+    setCourses([...courses, newCourse]);
   });
 
   const deleteCourse = async (courseId: string) => {
@@ -52,17 +52,17 @@ export default function Kanbas() {
 
   return (
     <Provider store={store}>
-    <div id="wd-kanbas" className="h-100">
-    <div className="d-flex h-100">
-      <div className="d-none d-md-block bg-black">
+      <div id="wd-kanbas" className="h-100">
+        <div className="d-flex h-100">
+          <div className="d-none d-md-block bg-black">
             <KanbasNavigation />
-       </div>
-        <div className="wd-main-content-offset p-3">
+          </div>
+          <div className="wd-main-content-offset p-3">
 
             <Routes>
               <Route path="/" element={<Navigate to="Dashboard" />} />
-              <Route path="Account/*" element={<Account/>} />
-               <Route path="Dashboard" element={
+              <Route path="Account/*" element={<Account />} />
+              {/* <Route path="Dashboard" element={
                           <ProtectedRoute><Dashboard
                           courses={courses}
                           course={course}
@@ -70,29 +70,30 @@ export default function Kanbas() {
                           addNewCourse={addNewCourse}
                           deleteCourse={deleteCourse}
                           updateCourse={updateCourse}/></ProtectedRoute>
-                } /> 
-       {/* <Route path="Dashboard" element={
-                       <Dashboard
-                          courses={courses}
-                          course={course}
-                          setCourse={setCourse}
-                          addNewCourse={addNewCourse}
-                          deleteCourse={deleteCourse}
-                          updateCourse={updateCourse}/>
-                } /> */}
-       
-       {/* <Route path="Courses/*" element={
-        <Courses courses={courses} /> }/> */}
-                <Route path="Courses/*" element={<ProtectedRoute>
-               <Courses courses={courses} /> </ProtectedRoute>} />  
+                } />  */}
+              <Route path="Dashboard" element={
+                <Dashboard
+                  courses={courses}
+                  course={course}
+                  setCourse={setCourse}
+                  addNewCourse={addNewCourse}
+                  deleteCourse={deleteCourse}
+                  updateCourse={updateCourse} />
+              } />
+
+              <Route path="Courses/*" element={
+                <Courses courses={courses} />} />
+              {/* <Route path="Courses/*" element={<ProtectedRoute>
+                <Courses courses={courses} /> </ProtectedRoute>} /> */}
               <Route path="Calendar" element={<h1>Calendar</h1>} />
               <Route path="Inbox" element={<h1>Inbox</h1>} />
             </Routes>
-     
-    </div>
-    </div>
-    </div>
+
+          </div>
+        </div>
+      </div>
     </Provider>
-);}
+  );
+}
 
 
